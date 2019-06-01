a7e343bed8e06f17ef1938682c1ed09d681c04b7
1 <Golzitskynikolay@gmail.com>
2019-06-01 19:22:18 +0300

    more comments

--- AVL-tree.c
+++ AVL-tree.c
@@ -4,124 +4,187 @@
 #include <malloc.h>
 #include <stdlib.h>
 
-// Используется, чтобы отслеживать последний пройденный узел с ненулевым коэффициентом сбалансированности,
-// чтобы после вставки элемента сбалансировать дерево.
-struct avl_node **v;
-
-// Последний пройденный узел с ненулевым коэффициентом сбалансированности
-struct avl_node *x;
-
-// Потомок x, который содержит вставленный узел
-struct avl_node *w;
-
-// Узел, который добавили
-struct avl_node *y;
-
-// Узел, хранящий значение y
-struct avl_node *z;
-
+/*
+ * Функция создания узла дерева.
+ */
 struct avl_node *createNode(struct avl_tree *tree, char *data) {
-    struct avl_node *node = malloc(sizeof *node); // выделяем память под узел дерева
+    struct avl_node *node = malloc(sizeof *node);  // выделяем память под узел дерева
     node->data = data;
-    node->link[0] = NULL; // создаём пустых потомков
+    node->link[0] = NULL;  // создаём пустых потомков
     node->link[1] = NULL;
-    node->balance = 0;    // изначально коэффициент сбалансированности = 0
-    tree->count++;       // увеличиваем количество узлов в дереве на 1
+    node->balance = 0;     // изначально коэффициент сбалансированности = 0
+    tree->count++;         // увеличиваем количество узлов в дереве на 1
     return node;
 };
 
 /*
  * Поиск подходящей позиции и последующая вставка элемента.
+ * Здесь tree->z - это текущий узел дерева, а tree->y - следующий.
  */
 int searchPlaceForInsert(char *data, struct avl_tree *tree, int isString) {
-    struct avl_node *nextNode = y;
-    struct avl_node *node = z;
 
     while (1) {
         int rightOrLeft;
 
         // Если такой элемент уже есть в дереве, то функцию можно завершить.
-        if ((isString == 1 && strcmp(data, node->data) == 0) || (isString == 0 && atof(data) == atof(node->data)))
-            return 2;
+        if ((isString == 1 && strcmp(data, tree->z->data) == 0) ||
+        (isString == 0 && atof(data) == atof(tree->z->data))) return 2;
 
-        // Сравниваем "rightOrLeft" со значением текущего node, если node больше, то rightOrLeft = 0
-        //(идем влево от node), а иначе rightOrLeft = 1 (идем вправо от node).
+        // Сравниваем "rightOrLeft" со значением текущего узла, если узел больше, то rightOrLeft = 0
+        //(идем влево от node), а иначе rightOrLeft = 1 (идем вправо от узла).
         if (isString == 1) {
-            if (strcmp(data, node->data) == -1) {
+            if (strcmp(data, tree->z->data) == -1) {
                 rightOrLeft = 0;
             } else {
-                rightOrLeft = (strcmp(data, node->data));
+                rightOrLeft = (strcmp(data, tree->z->data));
             }
         } else {
-            rightOrLeft = (atof(data) > atof(node->data));
+            rightOrLeft = (atof(data) > atof(tree->z->data));
         }
 
-        nextNode = node->link[rightOrLeft];
+        tree->y = tree->z->link[rightOrLeft];
 
         // В случае если y == NULL, то происходит вставка элемента в дерево
-        if (nextNode == NULL) {
-            nextNode = node->link[rightOrLeft] = createNode(tree, data);
-            if (nextNode == NULL) return 0;
+        if (tree->y == NULL) {
+            tree->y = tree->z->link[rightOrLeft] = createNode(tree, data);
+            if (tree->y == NULL) return 0;
             break;
         }
 
         // Отслеживаем последний пройденный узел с ненулевым коэффициентом сбалансированности,
-        // чтобы после вставки элемента сбалансировать дерево (если коэффициент сбалансированности равен 0,
-        // то при вставке нового узла он изменится на +1 или -1, и в этом случае балансировка не потребуется.
-        // Если же коэффициент изначально не равен 0, то после вставки узла дерево придется балансировать).
-        if (nextNode->balance != 0) {
-            v = &node->link[rightOrLeft];
-            x = nextNode;
+        // чтобы после вставки элемента сбалансировать дерево.
+        // Если коэффициент сбалансированности равен 0, то при вставке нового узла он изменится на +1 или -1,
+        // и в этом случае балансировка не потребуется. Если же коэффициент изначально не равен 0, то после
+        // вставки узла дерево придется балансировать.
+        if (tree->y->balance != 0) {
+            tree->v = &tree->z->link[rightOrLeft];
+            tree->x = tree->y;
         }
-        node = nextNode;
+        tree->z = tree->y;
     }
-    y = nextNode;
-    z = node;
     return 0;
 }
 
 /*
  * Пересчет коэффициентов сбалансированности для узлов, затронутых вставкой
  */
-void recalculateCoefficientBalance(char *data, int isString) {
+void recalculateCoefficientBalance(char *data, struct avl_tree *tree, int isString) {
     if (isString == 1) {
-        if (strcmp(data, x->data) == -1) {
-            z = x->link[0];
-            z->balance--;
-            w = z;
+        if (strcmp(data, tree->x->data) == -1) {
+            tree->z = tree->x->link[0];
+            tree->w = tree->z;
         } else {
-            z = x->link[strcmp(data, x->data)];
-            z->balance++;
-            w = z;
+            tree->z = tree->x->link[strcmp(data, tree->x->data)];
+            tree->w = tree->z;
         }
     } else {
-        int rightOrLeft = atof(data) > atof(x->data);
-        z = x->link[rightOrLeft];
-        if (rightOrLeft == 1) {
-            z->balance++;
-        } else {
-            z->balance--;
-        }
-        w = z;
+        int rightOrLeft = atof(data) > atof(tree->x->data);
+        tree->z = tree->x->link[rightOrLeft];
+        tree->w = tree->z;
     }
 
-    // z принимает значение y
-    while (z != y)
-        if ((isString == 1 && strcmp(data, z->data) == -1) || (isString == 0 && (atof(data) < atof(z->data)))) {
-            z->balance = -1;     // если вставленный элемент левый потомок z
-            z = z->link[0];
+    while (tree->z != tree->y)
+        if ((isString == 1 && strcmp(data, tree->z->data) == -1) ||
+        (isString == 0 && (atof(data) < atof(tree->z->data)))) {
+            tree->z->balance = -1;     // если вставленный элемент левый потомок z
+            tree->z = tree->z->link[0];
         } else {
-            z->balance = +1;    // если вставленный элемент правый потомок z
-            z = z->link[1];
+            tree->z->balance = +1;    // если вставленный элемент правый потомок z
+            tree->z = tree->z->link[1];
         }
 }
 
+/*
+ *  Балансировка при добавлении нового узла в левое поддерево, то есть дерево имеет следующий вид:
+ *  ------------------------------------------------------------------------------------------
+ *              |v                                        |v
+ *           -2(x)                                      0(w)
+ *            /   \                                     /   \
+ *        -1(w)     C   __h-2        ==>               A     (x)0
+ *         /   \                                      /     /  \
+ *        A     B     ___h-1                       (y)    B     C  _________h
+ *        |
+ *       (y)   _________h
+ *
+ * --------------------------------------------------------------------------------------------
+ *              |v                                         |v
+ *           -2(x)                                        (y)
+ *            /   \                                     /     \
+ *        +1(w)     D   __h-2        ==>              (w)     (x)
+ *         /   \                                      / \     /  \
+ *       A   -1(y)     ___h-1                       A    B   C    D
+ *            /   \
+ *           B     C  _____h
+ * --------------------------------------------------------------------------------------------
+ */
+void leftBalancing(struct avl_tree *tree) {
+    if (tree->x->balance != -1) {
+        tree->x->balance--;
+    } else if (tree->w->balance == -1) {
+        *tree->v = tree->w;                      // x->balance = -2 и w->balance = -1 => делаем балансировку для 1 случая
+        tree->x->link[0] = tree->w->link[1];
+        tree->w->link[1] = tree->x;
+        tree->x->balance = tree->w->balance = 0;
+    } else {                         // x->balance = -2 и w->balance = +1 => делаем балансировку для 2 случая
+        *tree->v = tree->z = tree->w->link[1];   // v и z = y
+        tree->w->link[1] = tree->z->link[0];     // w->link[1] = B
+        tree->z->link[0] = tree->w;              // y->link[0] = w
+        tree->x->link[0] = tree->z->link[1];     // x->link[0] = C
+        tree->z->link[1] = tree->x;              // y->link[1] = x
+        if (tree->z->balance == -1) {
+            tree->x->balance = 1;
+            tree->w->balance = 0;
+        } else if (tree->z->balance == 0) {
+            tree->x->balance = 0;
+            tree->w->balance = 0;
+        } else {
+            tree->x->balance = 0;
+            tree->w->balance = -1;
+        }
+        tree->z->balance = 0;
+    }
+}
+
+/*
+ * Балансировка при добавлении нового узла в правое поддерево (противоположно leftBalancing)
+ */
+void rightBalance(struct avl_tree *tree) {
+    if (tree->x->balance != +1) {
+        tree->x->balance++;
+    } else if (tree->w->balance == +1) {
+        *tree->v = tree->w;                       // x->balance = +2
+        tree->x->link[1] = tree->w->link[0];
+        tree->w->link[0] = tree->x;
+        tree->x->balance = tree->w->balance = 0;
+    } else {                          // x->balance = +2
+        *tree->v = tree->z = tree->w->link[0];
+        tree->w->link[0] = tree->z->link[1];
+        tree->z->link[1] = tree->w;
+        tree->x->link[1] = tree->z->link[0];
+        tree->z->link[0] = tree->x;
+        if (tree->z->balance == +1) {
+            tree->x->balance = -1;
+            tree->w->balance = 0;
+        } else if (tree->z->balance == 0)
+            tree->x->balance = tree->w->balance = 0;
+        else {
+            tree->x->balance = 0;
+            tree->w->balance = 1;
+        }
+        tree->z->balance = 0;
+    }
+}
+
+/*
+ * Вставка элемента в дерево.
+ */
 int insert(struct avl_tree *tree, char *data, int isString) {
 
-    // Если в дереве еще нет узлов
-    v = &tree->root;
-    x = z = tree->root;
-    if (x == NULL) {
+    tree->v = &tree->root;
+    tree->x = tree->root;
+    tree->z = tree->root;
+
+    if (tree->x == NULL) {    // Если в дереве еще нет узлов
         tree->root = createNode(tree, data);
         return tree->root != NULL;
     }
@@ -130,77 +193,14 @@ int insert(struct avl_tree *tree, char *data, int isString) {
     if (searchPlaceForInsert(data, tree, isString) == 2) return 0;
 
     // Пересчет коэффициентов сбалансированности для узлов, затронутых вставкой
-    recalculateCoefficientBalance(data, isString);
+    recalculateCoefficientBalance(data, tree, isString);
 
-// Балансировка при добавлении нового узла в левое поддерево, то есть дерево имеет следующий вид:
-//             |                                         |
-//          -2(x)                                      0(w)
-//           /   \                                     /   \
-//       -1(w)     C   __h-2        ==>               A     (x)0
-//        /   \                                      /     /  \
-//       A     B     ___h-1                       (y)    B     C  _________h
-//       |
-//      (y)   _________h
-
-    if ((isString == 1 && strcmp(data, x->data) == -1) || (isString == 0 && (atof(data) < atof(x->data)))) {
-        struct avl_node **v1 = v;
-        struct avl_node *x1 = x;
-        struct avl_node *w1 = w;
-        if (x->balance != -1) {
-            x->balance--;                // x->balance = 0,
-        } else
-            if (w->balance == -1) {
-            *v = w;                      // x->balance = -2 и w->balance = -1 => делаем балансировку для 1 случая
-            x->link[0] = w->link[1];
-            w->link[1] = x;
-            x->balance = w->balance = 0;
-        } else {                         // x->balance = -2 и w->balance = +1 => делаем балансировку для 2 случая
-            *v = z = w->link[1];
-            w->link[1] = z->link[0];
-            z->link[0] = w;
-            x->link[0] = z->link[1];
-            z->link[1] = x;
-            if (z->balance == -1) {
-                x->balance = 1;
-                w->balance = 0;
-            } else if (z->balance == 0)
-                x->balance = w->balance = 0;
-            else {
-                x->balance = 0;
-                w->balance = -1;
-            }
-            z->balance = 0;
-        }
-
+    if ((isString == 1 && strcmp(data, tree->x->data) == -1) ||
+    (isString == 0 && (atof(data) < atof(tree->x->data)))) {
+        leftBalancing(tree);  // Балансировка при добавлении нового узла в левое поддерево
+    } else {
+        rightBalance(tree);   // Балансировка при добавлении нового узла в правое поддерево
     }
 
-/* фрагмент № 4. Балансировка при добавлении нового узла в правое поддерево */
-    else {
-        if (x->balance != +1) {
-            x->balance++;
-        }else
-            if (w->balance == +1) {
-            *v = w;
-            x->link[1] = w->link[0];
-            w->link[0] = x;
-            x->balance = w->balance = 0;
-        } else {
-            *v = z = w->link[0];
-            w->link[0] = z->link[1];
-            z->link[1] = w;
-            x->link[1] = z->link[0];
-            z->link[0] = x;
-            if (z->balance == +1) {
-                x->balance = -1;
-                w->balance = 0;
-            } else if (z->balance == 0)
-                x->balance = w->balance = 0;
-            else {
-                x->balance = 0;
-                w->balance = 1;
-            }
-            z->balance = 0;
-        }
-    }
     return 1;
 }
--- main.c
+++ main.c
@@ -5,29 +5,33 @@
 #include <string.h>
 #include <malloc.h>
 
+/*
+ * Функция печати дерева либо в файл, если он подаётся, либо на экран, если файл = NULL
+ * Дерево перевёрнуто на 90 градусов влево.
+ */
 void printTree(struct avl_node *node, int n, struct avl_tree *tree, FILE *output) {
-    if (node->link[1]) {
-        printTree(node->link[1], n + 3, tree, output);
+    if (node->link[1] != NULL) {
+        printTree(node->link[1], n + 3, tree, output); // ищем самый большой элемент и затем выводим его
     }
     for (int i = 0; i < n; i++) {
         if (output == NULL) {
-            printf(" ");
+            printf(" ");          // вывод на экран
         } else {
-            fputs(" ", output);
+            fputs(" ", output);   // вывод в файл
         }
     }
 
     if (output == NULL) {
-        printf(" %s\n", node->data);
+        printf(" %s\n", node->data);  // вывод на экран
     } else {
-        fputs(" ", output);
+        fputs(" ", output);           // вывод в файл
         fputs(node->data, output);
         fputs("\n", output);
 
     }
 
-    if (node->link[0]) {
-        printTree(node->link[0], n + 3, tree, output);
+    if (node->link[0] != NULL) {
+        printTree(node->link[0], n + 3, tree, output);   // выводим элементы в порядке убывания
     }
 }
 
@@ -49,12 +53,12 @@ int main(int arg, char *str[]) {
 
     FILE *output = fopen(str[2], "wt");
 
-    struct avl_tree *tree = malloc(sizeof *tree);
+    struct avl_tree *tree = malloc(sizeof *tree);  // создаём пустое AVL-дерево
     tree->root = NULL;
     tree->count = 0;
 
-    char symbol;
-    char *string = calloc(20, sizeof(char));
+    char symbol;   // символ, считываемый из файла
+    char *string = calloc(20, sizeof(char));  // массив считанных символов (выделили память под 20 символов)
 
     int isString = 0;   // проверка на то что поступает, строка или число
     int checkForDouble = 0;  // проверка на double
@@ -63,29 +67,32 @@ int main(int arg, char *str[]) {
     int i = 0;
 
     while (!feof(input)) {
-        symbol = (char) getc(input);                   // считываем символы из входного файла
+        symbol = (char) getc(input);      // считываем символы из входного файла
+
         if (!feof(input) && symbol != ' ' && symbol != '\n') {
             string[i] = symbol;
+
+            // если считываемое слово оказалось больше выделенной памяти, то увеличиваем память
             if (_msize(string) == strlen(string) + 1) {
                 char *copy = calloc(_msize(string) + _msize(string), sizeof(char));
                 strcpy(copy, string);
                 string = copy;
             }
+
             if (i == 0 && symbol == '-') {
-                checkForMinus = 1;
+                checkForMinus = 1; // проверяем что поступает, отрицательное число или строка
             }
             if (checkForDouble == 1 && string[i - 1] != '.' && string[i + 1] != '.') {
-                isString = 0;
+                isString = 0;  // поступила строка, а не число, с плавающей точкой
             }
             if (isString == 0 && symbol == '.' && i != 0) {
-                checkForDouble = 1;
+                checkForDouble = 1;  // на следующем шаге проверим что поступает, число с плавающей точкой или строка
             }
             if (((checkForMinus == 0) &&
                  (isString == 0 && symbol != '0' && symbol != '1' && symbol != '2' && symbol != '3' &&
                   symbol != '4' && symbol != '5' && symbol != '6' && symbol != '7' && symbol != '8' &&
                   symbol != '9')) && checkForDouble == 0) {
-                printf("these are strings!\n\n");
-                isString = 1;
+                isString = 1;       // поступают строки
                 checkForDouble = 0;
                 checkForMinus = 0;
             }
@@ -95,22 +102,19 @@ int main(int arg, char *str[]) {
             i++;
         } else if (i != 0) {
             if (string[i - 1] == '.' || string[i - 1] == '-') {
-                printf("these are strings!\n\n");
-                isString = 1;
+                isString = 1; // в случае, если полседний символ был "." или "-", то поступают строки
                 checkForDouble = 0;
             }
-            char *copy = calloc(_msize(string), sizeof(char));
-            insert(tree, strcpy(copy, string), isString);
+            insert(tree, string, isString);       // вставка элемента в AVL-дерево
             i = 0;
-            free(string);
-            string = calloc(20, sizeof(char));
+            string = calloc(20, sizeof(char));    // количество выделенной памяти снова становится = 20
         }
     }
 
     printTree(tree->root, 0, tree, NULL);
     printTree(tree->root, 0, tree, output);
 
-    free(tree);
+    free(tree);   // освобождение памяти, выделенной под AVL-дерево
 
     fclose(input);
     fclose(output);
